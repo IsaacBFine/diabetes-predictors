44656cfe0932433b4b221734cd400abdbcfecd32
pduvergne <pduvergne@g.ucla.edu>
2026-06-11 20:41:44 -0700

    Update PLSR_final_model.py

--- Code/PLSR_final_model.py
+++ Code/PLSR_final_model.py
@@ -19,3 +19,21 @@ x_loadings = pd.DataFrame(pls_final.x_loadings_, index=X.columns, columns=[f'Com
 #Get Y loadings
 y_loadings = pd.DataFrame(pls_final.y_loadings_,index=['Diabetes_binary'],columns=[f'Component {i+1}' for i in range(4)])
 
+
+plt.figure(figsize=(8, 6))
+
+#Plot feature variables
+plt.scatter(x_loadings['Component 1'], x_loadings['Component 2'], color='blue')
+for var in x_loadings.index:
+    plt.annotate(var, (x_loadings['Component 1'][var], x_loadings['Component 2'][var]), fontsize=8)
+
+#Plot diabetes
+plt.scatter(y_loadings['Component 1'], y_loadings['Component 2'], color='orange', s=400, marker='*', label='Diabetes')
+plt.annotate('Diabetes', (y_loadings['Component 1'].iloc[0], y_loadings['Component 2'].iloc[0]), fontsize=15, color='black', fontweight='bold')
+
+plt.axhline(0, color='grey', linestyle='--')
+plt.axvline(0, color='grey', linestyle='--')
+plt.xlabel('Component 1 Loadings')
+plt.ylabel('Component 2 Loadings')
+plt.title('PLSR Loadings: Component 1 vs Component 2')
+plt.show()
