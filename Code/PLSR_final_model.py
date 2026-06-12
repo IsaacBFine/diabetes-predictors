4a7a6f72762ff67e49a9ea66083d3bd333332a28
pduvergne <pduvergne@g.ucla.edu>
2026-06-11 20:40:29 -0700

    Update PLSR_final_model.py

--- Code/PLSR_final_model.py
+++ Code/PLSR_final_model.py
@@ -1,4 +1,13 @@
-
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.model_selection import cross_val_score
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+random.seed(67)
+import numpy as np
+np.random.seed(67)
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 #Fit a 4 component model
 pls = PLSRegression(n_components=4)
@@ -9,3 +18,4 @@ x_loadings = pd.DataFrame(pls_final.x_loadings_, index=X.columns, columns=[f'Com
 
 #Get Y loadings
 y_loadings = pd.DataFrame(pls_final.y_loadings_,index=['Diabetes_binary'],columns=[f'Component {i+1}' for i in range(4)])
+
